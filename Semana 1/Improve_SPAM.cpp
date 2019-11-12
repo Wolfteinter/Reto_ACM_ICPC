f8337e4ac3e0333e4988c96f7dfee48ff7596f8c
Wolfteinter <onderfra@gmail.com>
2019-11-11 20:46:39 -0600

    Se elimina el archivo que no era una solucion

--- Semana 1/Improve_SPAM.cpp
+++ Semana 1/Improve_SPAM.cpp
@@ -1,26 +1,35 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long int lli;
 lli MAX = 1000000007;
 lli V,m;
-vector<lli> *lista;
-lli msj = 0;
+vector<lli> lista[3000];
 unordered_set<lli> setT;
-void dfs(lli v){
+lli DP[3000];
+lli dfs(lli v){
+    lli msj = 0;
     for(int i = 0;i<lista[v].size();i++){
-        if(lista[v][i]<=m-1)dfs(lista[v][i]);
-        else{msj++; msj=msj%MAX;setT.insert(lista[v][i]);};
+        if(lista[v][i]<=m-1){
+            if(DP[lista[v][i]] == 0){
+                msj = (msj + dfs(lista[v][i]))%MAX;
+            }else msj = (msj + DP[lista[v][i]])%MAX;
+        }else{
+            msj = (msj + 1)%MAX;
+            setT.insert(lista[v][i]);
+        }
     }
+    DP[v] = (DP[v] + msj)%MAX;
+    return msj;
 }
 int main(){
     lli x,a;
     cin>>V>>m;
-    lista = new vector<lli>[m];
+    memset(DP,0,sizeof(DP));
     for(int i = 0;i<m;i++){
         cin>>x;
         while(x--){cin>>a;lista[i].push_back(a-1);}
     }
     dfs(0);
-    cout<<msj%MAX<<" "<<setT.size()<<endl;
+    cout<<DP[0]%MAX<<" "<<setT.size()<<endl;
     return 0;
 }
